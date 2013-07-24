383cf24cc752c28b6650bab8c16ac321811f2f4f
Jean-Christophe Cuvelier <jcc@cuvelier.be>
2013-07-24 10:51:38 +0200

    Add DebugArea

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,5 +2,23 @@
 
 $lang = array(
   'installpostmessage' => 'Debug Switch has succesfuly been installed.',
-  'help' => 'This module allows you to activate/deactivate the debug mode very easily.',
+  'help' => '<p>This module allows you to activate/deactivate the debug mode very easily.</p>
+	
+	<p>Sometimes the config.php cannot be edited. In this case, add those lines:</p>
+	<textarea>
+	 $config[\'debug\'] = false;
+	    // --DEBUGSWITCH--
+	    if(isset($_SESSION[\'debug\']))
+	    {
+	      $config[\'debug\'] = true;
+	    }
+	</textarea>
+
+	<h3>DebugArea</h3>
+	<p>You can use the tag DebugArea to only show some part when debug is activated.</p>
+	<p>Example:</p>
+	<textarea>
+	    {DebugArea}My Test String{/DebugArea}
+	</textarea>
+	',
   );
